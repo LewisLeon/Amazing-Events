8211caa97904766c5f6e983fba6d545ea2cba620
Angel <lewisleon3@gmail.com>
2023-03-12 21:50:18 -0300

    Task 3

--- scripts/Past.js
+++ scripts/Past.js
@@ -3,10 +3,10 @@ let PastContent = document.getElementById("main")
 let cont = ""
 
 eventos.EventPast.forEach((evento) => {
-    cont += `<div id="1" class="card m-4 cards hs" style="width: 18rem;">
+    cont += `<div id="${evento._id}" class="card m-4 cards hs" style="width: 18rem;">
     <img src=${evento.image} class="card-img-top p-2" alt="img Cinema">
     <div class="card-body fondoTex">
-        <h5 class="card-title ">${evento.name}</h5>
+        <h5 class="card-title name">${evento.name}</h5>
         <p class="card-text align-content-between">${evento.description}</p>
         <div class="cardS align-content-end">
             <p>Price: ${evento.price}$</p>
